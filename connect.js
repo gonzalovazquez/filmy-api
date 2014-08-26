e47b9b06165d2c3a075be39519e15b52a7379d79
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-08-26 08:48:06 -0400

    added CORS to enable cross-browser communication

--- connect.js
+++ connect.js
@@ -5,23 +5,33 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
 	extended: true}));
 
 app.use(express.static('public'));
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 //Getting Data
-app.get('/api/show', function(req, res) {
-	db.movies.find((function(err, movie) {
+app.get('/', function(req, res) {
+	db.movies.find((function(err, movie) {		
 		res.send(movie.map(function(item, i) {
 			console.log(item);
+			return item;
 		}));
 	}));
 });
 
 //Storing DATA
-app.post('/api/save', function(req, res) {
+app.post('/', function(req, res) {
 	db.movies.save(req.body, callback);
 
 	function callback(err, saved) {
@@ -34,6 +44,6 @@ app.post('/api/save', function(req, res) {
 	}
 });
 
-var server = app.listen(15365, function() {
-    console.log('Listening on port %d', server.address().port);
+var server = app.listen(15715, function() {
+    console.log('CORS-enabled web server listening on port %d', server.address().port);
 });
