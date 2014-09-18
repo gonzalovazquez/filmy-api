130271322470bce438c1f7f0e43d29b748b651a0
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-09-18 09:24:39 -0400

    removed authentication ode

--- filmy.js
+++ filmy.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({
 	extended: true}));
 
-app.use(express.static('public'));
+app.use(express.static('../filmy/public'));
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
