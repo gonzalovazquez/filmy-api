a2e35830209d7d37656416d1680cae4cf7aec620
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-04-03 18:24:38 -0400

    fixed indentation

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var mocha = require('gulp-mocha');
 
 gulp.task('default', function() {
 	return gulp.src('tests/app.spec.js', { read: false })
-			.pipe(mocha({ reporter:'nyan' }))
-			.once('error', function() {
-				process.exit(1);
-			});
+		.pipe(mocha({ reporter:'nyan' }))
+		.once('error', function() {
+			process.exit(1);
+		});
 });
